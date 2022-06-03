2f645e02cbd19b13a7265506cffeaf137e7cd5dc
fpt-huypt13 <96227275+fpt-huypt13@users.noreply.github.com>
2022-06-04 00:12:05 +0700

    feat:add cors

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 const express = require("express");
 const shortid = require("shortid");
+const cors = require("cors");
 
 const ServerHandle = require("./src/ServerHandle");
 const Database = require("./src/database/Database");
@@ -39,6 +40,7 @@ io.on("connection", (socket) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world!");
